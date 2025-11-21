001e088be51757755eaacbba5f8b981cfcda9f76
JAEJUN090210 <ssokage090210@naver.com>
2025-11-21 16:23:23 +0900

    [Feat] #5_input_text 퍼블리싱
    [Feat] #5_input_text 퍼블리싱
    
    - [x] input_default -> input 박스
    - [x] input_text
    - [ ] input_record

---
+++ modules/xquare-user-interfaces/src/components/input/Input_text.tsx
@@ -0,0 +1,97 @@
+/** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled";
+import { Xquare_colors } from "../../styles/colors";
+
+// 타입정의
+interface InputProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  disabled?: boolean;
+  type?: "text" | "password" | "email" | "number";
+  width?: string;
+  height?: string;
+  title?: string;
+  titleColor?: string;
+}
+
+interface TitleProps {
+  titleColor?: string;
+}
+
+// 스타일 정의
+const Wraper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+`;
+
+const StyledInput = styled.input<InputProps>`
+  width: ${({ width }) => width || "300px"};
+  height: ${({ height }) => height || "50px"};
+
+  border: none;
+  border-bottom: 1.2px solid ${Xquare_colors.gray[500]};
+  background-color: ${Xquare_colors.white};
+
+  color: ${Xquare_colors.black};
+  font-size: 1.1rem;
+  font-weight: 400;
+  line-height: 1.1rem;
+
+  &::placeholder {
+    color: ${Xquare_colors.gray[500]};
+  }
+
+  &:focus {
+    outline: none;
+    border-bottom: 1.2px solid ${Xquare_colors.purple[500]};
+    color: ${Xquare_colors.purple[500]};
+
+    &::placeholder {
+      color: ${Xquare_colors.purple[500]};
+    }
+  }
+
+  &:disabled {
+    border-bottom: 1.2px solid ${Xquare_colors.gray[300]};
+
+    &::placeholder {
+      color: ${Xquare_colors.gray[300]};
+    }
+  }
+`;
+
+const Title = styled.span<TitleProps>`
+  font-size: 0.9rem;
+  font-weight: 500;
+  color: ${({ titleColor }) => titleColor || Xquare_colors.red[500]};
+`;
+
+// 컴포넌트
+export const Input_text: React.FC<InputProps> = ({
+  value,
+  onChange,
+  placeholder,
+  disabled = false,
+  type = "text",
+  width,
+  height,
+  title,
+  titleColor,
+}) => {
+  return (
+    <Wraper>
+      <StyledInput
+        width={width}
+        height={height}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        disabled={disabled}
+        type={type}
+      />
+      <Title titleColor={titleColor}>{title}</Title>
+    </Wraper>
+  );
+};
--- modules/xquare-user-interfaces/src/components/input/index.ts
+++ modules/xquare-user-interfaces/src/components/input/index.ts
@@ -1,1 +1,2 @@
 export { Input_default } from "./Input_default";
+export { Input_text } from "./Input_text";
