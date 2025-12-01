ea8f7777cbfd38f4e9493b7338f5d81de02a60ee
JAEJUN090210 <ssokage090210@naver.com>
2025-12-01 20:09:20 +0900

    Fixes: Corrects typo in typography export path
    Updates the export path for the typography component to resolve a typo, ensuring correct module resolution.

--- modules/xquare-user-interfaces/index.ts
+++ modules/xquare-user-interfaces/index.ts
@@ -1,4 +1,4 @@
-export * from "./src/components/Typography/index";
+export * from "./src/components/typography/index";
 export * from "./src/components/button/index";
 export * from "./src/components/input/index";
 export * from "./src/styles/colors";
