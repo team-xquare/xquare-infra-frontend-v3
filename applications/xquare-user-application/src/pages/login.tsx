37a56982a69ac1110c9c2b7d410e3c4c5fb0a980
JAEJUN090210 <ssokage090210@naver.com>
2025-12-05 16:41:03 +0900

    Sets up base application structure
    Establishes the foundational structure for the user application.
    
    - Updates the HTML title and favicon for branding
    - Adds basic routing
    - Imports base ui modules
    
    Relates to #15

---
+++ applications/xquare-user-application/src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <div></div>
+      <h1>Login Page</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
