98700cc4d5434073282b50b422dd86ef486827ee
JAEJUN090210 <ssokage090210@naver.com>
2025-12-10 22:55:10 +0900

    dependancy 제거
    dependancy 제거

--- applications/xquare-user-application/src/pages/Pagenotfound.tsx
+++ applications/xquare-user-application/src/pages/Pagenotfound.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Xquare_colors } from "@xquare/user-interfaces";
 
