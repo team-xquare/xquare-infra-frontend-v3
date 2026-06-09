32b6111be467faaf33f015aade79be571f2d4cdd
jaejun <ssokage090210@naver.com>
2026-06-09 18:55:41 +0900

    feature :: pwa remove
    pwa 제거

--- applications/xquare-admin-application/vite.config.ts
+++ applications/xquare-admin-application/vite.config.ts
@@ -9,20 +9,20 @@ export default defineConfig({
     alias: {
       "@xquare/hooks": path.resolve(
         __dirname,
-        "../../modules/xquare-hooks/index.ts"
+        "../../modules/xquare-hooks/index.ts",
       ),
       "@xquare/hooks/": path.resolve(__dirname, "../../modules/xquare-hooks/"),
       "@xquare/user-interfaces": path.resolve(
         __dirname,
-        "../../modules/xquare-user-interfaces/index.ts"
+        "../../modules/xquare-user-interfaces/index.ts",
       ),
       "@xquare/user-interfaces/": path.resolve(
         __dirname,
-        "../../modules/xquare-user-interfaces/"
+        "../../modules/xquare-user-interfaces/",
       ),
       "@xquare/utils": path.resolve(
         __dirname,
-        "../../modules/xquare-utils/index.ts"
+        "../../modules/xquare-utils/index.ts",
       ),
       "@xquare/utils/": path.resolve(__dirname, "../../modules/xquare-utils/"),
     },
--- applications/xquare-user-application/vite.config.ts
+++ applications/xquare-user-application/vite.config.ts
@@ -1,39 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: "Xquare Infra User",
-        short_name: "Xquare Infra",
-        description: "Xquare Infra V3 PWA Application",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "minimal-ui",
-        start_url: "/",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 
   resolve: {
     alias: {
